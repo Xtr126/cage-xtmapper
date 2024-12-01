29d8cd6574d9f382edcff945a4114e8966218f68
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2024-12-02 01:33:19 +0530

    feat: add CI build

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -v
 exit_error() {
     echo -e "$1"
     exit 1
@@ -5,6 +6,7 @@ exit_error() {
 
 extract_tarball() {
     local tarball="$1"
+    echo "Extracting $tarball"
     if ! tar xf "$tarball";  then
         bsdtar -xf "$tarball" || exit_error "Failed to extract tarball\nTried tar and bsdtar"
     fi
@@ -28,11 +30,12 @@ cd cage-0.2.0
 mkdir -p subprojects/
 
 apply_cage_patches() {
+    echo "Applying cage patches"
     for patch in "$parent_dir"/*.patch; do        
         patch -p1 -i "$patch"
     done
 }
-apply_cage_patches
+apply_cage_patches 
 
 (   
     cd subprojects; 
@@ -42,14 +45,16 @@ apply_cage_patches
 )
 
 apply_wlroots_patches() {
+    echo "Applying wlroots patches"
     for patch in "$parent_dir"/wlroots/*.patch; do        
         patch -d subprojects/wlroots/ -p1 -i "$patch"
     done
 }
 apply_wlroots_patches
 
-meson setup build --buildtype=release -Ddefault_library=static
+meson setup build --buildtype=release -Ddefault_library=static -Dprefix=/installed
 meson compile -C build
+meson install -C build --destdir "$parent_dir"/build
 
 
 
