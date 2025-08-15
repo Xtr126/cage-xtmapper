551c8694e07bab1397b7ae25eaad57fb42f130ed
Xtr126 <k.gunetilleke@gmail.com>
2025-08-15 21:47:25 +0530

    Avoid using sudo to launch cage_xtmapper.sh

--- cage_xtmapper.sh
+++ cage_xtmapper.sh
@@ -2,10 +2,6 @@
 
 while [ $# -gt 0 ]; do
     case "$1" in
-    --user)
-        shift
-        user="$1"
-        ;;
     --window-width)
         shift
         XTMAPPER_WIDTH="$1"
@@ -29,37 +25,24 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-if [ "$use_adb" != 1 ]; then
-    if [ "$(id -u)" != "0" ]; then
-        echo "This script requires root access to access waydroid shell."
-        exit 1
-    fi
-
-    if [[ -z "$user" ]]; then
-        echo "User not specified."
-        exit 1
-    fi
-    waydroid container stop
-else
-    sudo waydroid container stop
-fi
+waydroid session stop
 
 export XTMAPPER_WIDTH=${XTMAPPER_WIDTH:-1280}
 export XTMAPPER_HEIGHT=${XTMAPPER_HEIGHT:-720}
 BOOTCLASSPATH=/apex/com.android.art/javalib/core-oj.jar:/apex/com.android.art/javalib/core-libart.jar:/apex/com.android.art/javalib/okhttp.jar:/apex/com.android.art/javalib/bouncycastle.jar:/apex/com.android.art/javalib/apache-xml.jar:/system/framework/framework.jar:/system/framework/framework-graphics.jar:/system/framework/ext.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/apex/com.android.i18n/javalib/core-icu4j.jar:/apex/com.android.adservices/javalib/framework-adservices.jar:/apex/com.android.adservices/javalib/framework-sdksandbox.jar:/apex/com.android.appsearch/javalib/framework-appsearch.jar:/apex/com.android.btservices/javalib/framework-bluetooth.jar:/apex/com.android.conscrypt/javalib/conscrypt.jar:/apex/com.android.ipsec/javalib/android.net.ipsec.ike.jar:/apex/com.android.media/javalib/updatable-media.jar:/apex/com.android.mediaprovider/javalib/framework-mediaprovider.jar:/apex/com.android.ondevicepersonalization/javalib/framework-ondevicepersonalization.jar:/apex/com.android.os.statsd/javalib/framework-statsd.jar:/apex/com.android.permission/javalib/framework-permission.jar:/apex/com.android.permission/javalib/framework-permission-s.jar:/apex/com.android.scheduling/javalib/framework-scheduling.jar:/apex/com.android.sdkext/javalib/framework-sdkextensions.jar:/apex/com.android.tethering/javalib/framework-connectivity.jar:/apex/com.android.tethering/javalib/framework-connectivity-t.jar:/apex/com.android.tethering/javalib/framework-tethering.jar:/apex/com.android.uwb/javalib/framework-uwb.jar:/apex/com.android.wifi/javalib/framework-wifi.jar
 
-systemctl restart waydroid-container.service
+sudo waydroid container start
 
 if [ "$use_adb" != 1 ]; then
-    su "$user" --command "cage_xtmapper waydroid show-full-ui" | (
-        while [[ -z $(waydroid shell getprop sys.boot_completed) ]]; do
+    cage_xtmapper waydroid show-full-ui | (
+        while [[ -z $(sudo waydroid shell getprop sys.boot_completed) ]]; do
             sleep 1;
         done;
 
-        ABI=$(waydroid shell getprop ro.product.cpu.abi | tr -d '\r')
+        ABI=$(sudo waydroid shell getprop ro.product.cpu.abi | tr -d '\r')
 
         for pkg in "xtr.keymapper.debug" "xtr.keymapper"; do
-            APK_PATH=$(waydroid shell pm path "$pkg" 2>/dev/null | cut -d ':' -f 2 | tr -d '\r')
+            APK_PATH=$(sudo waydroid shell pm path "$pkg" 2>/dev/null | cut -d ':' -f 2 | tr -d '\r')
             if [ -n "$APK_PATH" ]; then
                 APK_DIR=$(dirname "$APK_PATH")
                 NATIVE_LIB_PATH="$APK_DIR/lib/$ABI"
@@ -73,7 +56,7 @@ if [ "$use_adb" != 1 ]; then
             exit 1
         fi
 
-        exec waydroid shell -- sh -c "exec env BOOTCLASSPATH=\$BOOTCLASSPATH:$BOOTCLASSPATH /system/bin/app_process \
+        exec sudo waydroid shell -- sh -c "exec env BOOTCLASSPATH=\$BOOTCLASSPATH:$BOOTCLASSPATH /system/bin/app_process \
             -Djava.library.path="$NATIVE_LIB_PATH" \
             -Djava.class.path="$APK_PATH" \
             / xtr.keymapper.server.RemoteServiceShell \
